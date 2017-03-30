a1b6e82240f0b91f0642fb203c4a530805a165a9
Allison Betancourt <unreasonablyniceee@gmail.com>
2017-03-30 09:58:40 -0500

    Initial styling (#1)
    * Update to readme and added reset. Also added new feature: JSON encoding
    
    * lists each node

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # Text Game Editor
-A simple text decision game builder. The builder produces a JSON file for your story that you can use for your own text game. See Data.md for sample struture. 
+A simple decision-based text game builder. The builder produces a JSON file for your story that you can use for your own decision-based text game.
+
+See DATA.md for sample structure.
+
+## Getting Started
+1. `npm install`
+2. If you have [http-server](https://www.npmjs.com/package/http-server) installed you can use `npm start` to serve files at `localhost:8080`
+3. Code away!
--- src/editor.js
+++ src/editor.js
@@ -1,16 +1,36 @@
-var story_session = {
-  story_meta: {},
+var storySession = {
+  storyMeta: {},
   story: []
 };
+
+const listItem = ({text, choicesA, choicesB}) => `
+  <li>
+    <form>
+      <div class="form-group">
+        <div class="col-md-8">
+          <textarea readonly class="hide-borders">
+            ${text}
+          </textarea>
+        </div>
+        <div class="col-md-1">
+            ${choicesA}
+        </div>
+        <div class="col-md-1">
+            ${choicesB}
+        </div>
+      </div>
+    </form>
+  </li>
+`;
 
 function updateTitle() {
   var title = $('#story-title').val();
   // Add meta data to meta
-  story_session.story_meta.title = title;
-  console.log(story_session);
+  storySession.storyMeta.title = title;
+  console.log(storySession);
 }
 function addNode() {
-  var id = story_session.story.length;
+  var id = storySession.story.length;
   var text = $('#story-text').val();
   var choiceA = $('#story-choice-A').val() || null;
   var choiceB = $('#story-choice-B').val() || null;
@@ -20,16 +40,16 @@ function addNode() {
     choices: [choiceA, choiceB]
   }
 
-  story_session.story.push(node);
-  console.log(story_session);
+  storySession.story.push(node);
+  console.log(storySession);
 }
 function formClear() {
-  $('#story-text').val('');
+  $('#story-text').val('').focus();
   $('#story-choice-A').val('NA');
   $('#story-choice-B').val('NA');
 }
 function addNodeOptions() {
-  var selOptions = story_session.story;
+  var selOptions = storySession.story;
   var starterOption = '<option value="NA" selected="selected">NA</option>'
   var selectA = $('#story-choice-A');
   var selectB = $('#story-choice-B');
@@ -51,18 +71,81 @@ function addNodeOptions() {
 }
 function displayOptions() {
   var listArea = $('#story-list');
-  var storyNodes = story_session.story;
+  var storyNodes = storySession.story;
+  var list = [];
+
+  // List of select options
+  var selectOptions = ['NA'];
+  for (var i = 0; i < storySession.story.length; i++) {
+    selectOptions.push(i);
+  }
+  console.log(selectOptions);
 
   listArea.empty();
 
-  if (story_session.story_meta.title) {
-    listArea.append('<h2>' + story_session.story_meta.title + '</h2>');
+  if (storySession.storyMeta.title) {
+    listArea.append('<h2>' + storySession.storyMeta.title + '</h2>');
   }
-  listArea.append('<ol class="story-list"></ol>');
+
+  listArea.append('<ul class="story-list"></ul>');
   storyNodes.forEach(function(elm) {
-    var node = '<li>' + elm.text + '</li>';
-    listArea.children('ol').append(node);
+    var text = elm.text;
+    var nodeA = elm.choices[0];
+    var nodeB = elm.choices[1];
+    var id = elm.id;
+    var optionA, optionB;
+    var optionsA = [];
+    var optionsB = [];
+
+    for (var i = 0; i < selectOptions.length; i++) {
+      if (selectOptions[i] == nodeA) {
+        optionA = '<option selected="selected" value="' + selectOptions[i] + '">' + selectOptions[i] + '</option>';
+      } else {
+        optionA = '<option value="' + selectOptions[i] + '">' + selectOptions[i] + '</option>';
+      }
+
+      if (selectOptions[i] == nodeB) {
+        optionB = '<option selected="selected" value="' + selectOptions[i] + '">' + selectOptions[i] + '</option>';
+      } else {
+        optionB = '<option value="' + selectOptions[i] + '">' + selectOptions[i] + '</option>';
+      }
+
+      optionsA.push(optionA);
+      optionsB.push(optionB);
+    }
+
+    list.push({id, text, optionsA, optionsB});
   });
+  list.forEach(function(elm) {
+    $('.story-list').append(
+      '<li data-editable data-key="' + elm.id + '"><form><div class="form-group"><div class="col-md-8"><textarea readonly class="hide-borders">' + elm.text + '</textarea></div><div class="col-md-1"><select>' + elm.optionsA.join('') + '</select></div><div class="col-md-1"><select>' + elm.optionsB.join('') + '</select></div></form></li>'
+    );
+  });
+}
+//export the storySession as a downloaded JSON file
+function exportStorySession(storySession) {
+  const downloadSelector = '#exportLink';
+  const myStorySessionString = JSON.stringify(storySession);
+  const b64StorySession = 'data:application/json;charset=utf-8,' + encodeURIComponent(myStorySessionString);
+
+  const downloadHref = document.querySelector(downloadSelector);
+  downloadHref.download = 'story_session.json';
+  downloadHref.href = b64StorySession;
+  downloadHref.click();
+}
+
+//return either an Error object or a decoded story session
+function importStorySession(storySessionText) {
+  const decodedStorySession = decodeURIComponent(storySessionText);
+  let storySession = null;
+
+  try {
+    storySession = JSON.parse(decodedStorySession);
+  } catch (e) {
+    return e;
+  }
+
+  return storySession;
 }
 
 $('#form-title').submit(function(e) {
@@ -76,4 +159,14 @@ $('#form-story-node').submit(function(e) {
   formClear();
   addNodeOptions();
   displayOptions();
+});
+$('#click-download').click(function(e) {
+  e.preventDefault();
+
+  if (storySession.story.length !== 0) {
+    console.log(storySession.story);
+    exportStorySession(storySession);
+  } else {
+    console.log('no story to download');
+  }
 });
